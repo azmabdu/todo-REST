951a5cf16ba71dbd63d765b02c09d09557b16130
user <abduvohidovazim885@gmail.com>
2023-01-28 19:31:58 +0500

    build

---
+++ todofront/build/precache-manifest.08fee0915f7ec473b9836bc506edd623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68d1cc1f2a582d917ebd",
+    "url": "/static/css/main.e3836d8a.chunk.css"
+  },
+  {
+    "revision": "68d1cc1f2a582d917ebd",
+    "url": "/static/js/main.68d1cc1f.chunk.js"
+  },
+  {
+    "revision": "f7eef820811137717d72",
+    "url": "/static/js/1.f7eef820.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bfd569b4a2a738cf6c70b51fd76109c",
+    "url": "/index.html"
+  }
+];
